3510199a3f7cc97dad578781e947a1187134bda8
Daniel Davis <danielbdavis@gmail.com>
2016-09-09 23:00:53 -0700

    Cleanup

--- chat/serializers.py
+++ chat/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 from .models import Message
 from .models import Room
 
-class MessageSerializer(serializers.ModelSerializer):
-    # owner = serializers.ReadOnlyField(source='owner.username')
-    
-    class Meta:
-        model = Message
-        fields = (
-            'id',
-            'room',
-            'handle',
-            'message',
-            'timestamp')
+# class MessageSerializer(serializers.ModelSerializer):
+#     # owner = serializers.ReadOnlyField(source='owner.username')
+#
+#     class Meta:
+#         model = Message
+#         fields = (
+#             'id',
+#             'room',
+#             'handle',
+#             'message',
+#             'timestamp')
 
 class RoomSerializer(serializers.ModelSerializer):
     # owner = serializers.ReadOnlyField(source='owner.username')
--- chat/urls.py
+++ chat/urls.py
@@ -4,27 +4,17 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from . import views
 
-# # Serializers define the API representation.
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('url', 'username', 'email', 'is_staff')
-#
-# # ViewSets define the view behavior.
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
 router.register(r'rooms', RoomViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    
     url(r'^$',  views.about, name='about'),
     url(r'^admin/', admin.site.urls),
     
-    url(r'^', include(router.urls)),
+    
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     
     url(r'^new/$', views.new_room, name='new_room'),
